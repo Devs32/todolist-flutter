01310a7b4f3fe09d623f4626d67fcf0b3f67de24
hanseok kim <hasuikhs@gmail.com>
2023-08-02 21:47:27 +0900

    Add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 /// Flutter code sample for [PageView].
 
-void main() => runApp(const PageViewExampleApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Future.delayed(const Duration(seconds: 10));
+  FlutterNativeSplash.remove();
+
+  runApp(const PageViewExampleApp());
+}
 
 class PageViewExampleApp extends StatelessWidget {
   const PageViewExampleApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('PageView Sample')),
-        body: const PageViewExample(),
-      ),
-    );
-  }
-}
-
-class PageViewExample extends StatelessWidget {
-  const PageViewExample({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final PageController controller = PageController(initialPage: 1);
-    return PageView(
-      /// [PageView.scrollDirection] defaults to [Axis.horizontal].
-      /// Use [Axis.vertical] to scroll vertically.
-      controller: controller,
-      children: const <Widget>[
-        Center(
-          child: Text('First Page'),
-        ),
-        Center(
-          child: Text('Second Page'),
-        ),
-        Center(
-          child: Text('Third Page'),
-        ),
-      ],
+    return const MaterialApp(
+      home: Scaffold(),
     );
   }
 }
